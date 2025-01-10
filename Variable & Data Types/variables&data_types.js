4672937d511488ed6a64c259c228be2d3b979dcb
susmitaaryal <susmitaaryal69@gmail.com>
2025-01-10 14:44:20 +0545

    Add files via upload

---
+++ Variable & Data Types/variables&data_types.js
@@ -0,0 +1,41 @@
+console.log("susmita aryal");
+
+name = "susmita aryal";
+console.log(name);
+
+age = 24;
+console.log(age);
+
+x=null;
+console.log(x);
+
+y=undefined;
+console.log(y);
+
+isFollow=true;
+console.log(isFollow);
+
+let age = 24;
+age = 25;
+age = 26;
+console.log(age);
+
+{
+    let a = 5;
+    console.log(a);
+}
+
+{
+    let a=10;
+    console.log(a);
+}
+
+const student = {
+    name: "susmita aryal",
+    age: 20,
+    cgpa:8.2,
+    ispass:true,
+
+};
+
+console.log(student["cgpa"]);
---
+++ Variable & Data Types/variables&data_types_practice.js
@@ -0,0 +1,23 @@
+//create a const object called "product" to store information shown in the picture.
+
+const product ={
+    title: "ball pen",
+    rating: 4,
+    offer: 5,
+    price:270,
+};
+
+console.log(product)
+
+
+//create a const object called "profile" to store information shown in the picture.
+
+const profile={
+    id: "shradhakhapra",
+    post:195,
+    followers:569,
+    following:4,
+    bio:"apna college | ex- microsoft, drdo",
+};
+
+console.log(typeof profile["username"]);
