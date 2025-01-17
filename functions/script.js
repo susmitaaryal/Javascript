f6a39924ea84571b11b51e6f03d21311ee2c4dbc
susmitaaryal <susmitaaryal69@gmail.com>
2025-01-17 16:01:27 +0545

    Add files via upload

---
+++ functions/script.js
@@ -0,0 +1,24 @@
+function myfunction(){
+    console.log("welcome to susmita aryal");
+    console.log("we are learning javascript");
+}
+
+myfunction();
+myfunction();
+
+function moifunction(msg){
+    console.log(msg);
+}
+moifunction("i love js");
+
+//sum of two numbers
+
+function sum (x,y){
+    s = x+y;
+    return s;
+}
+let val = sum(3,4);
+console.log(val);
+
+
+
